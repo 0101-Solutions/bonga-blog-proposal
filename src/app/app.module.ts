1813d59df7f02b2e865d13cb9746721725a85ba9
Toxic254 <cyrus_g@outlook.com>
2021-04-14 17:24:56 +0300

    Successfully displayed ai component on home page increasing components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './_app/app.component';
 import { AiComponent } from './ai/ai.component';
+import { NetworkingComponent } from './networking/networking.component';
+import { CybersecurityComponent } from './cybersecurity/cybersecurity.component';
 // We moved the first app into a component of is own.
 
 // This is the app.js {NODEJS} of your angular file. It is where the application is rendered.
@@ -12,7 +14,9 @@ import { AiComponent } from './ai/ai.component';
   // After using the ng g c <component-name> our components will be enlisted here automatically
   declarations: [
     AppComponent,
-    AiComponent
+    AiComponent,
+    NetworkingComponent,
+    CybersecurityComponent
   ],
   // This is where our application will bring in the imports necessary for our app to run.
   imports: [
---
+++ src/app/cybersecurity/cybersecurity.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cybersecurity',
+  templateUrl: './cybersecurity.component.html',
+  styleUrls: ['./cybersecurity.component.css']
+})
+export class CybersecurityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
