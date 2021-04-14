f82fe2a05f9b2e2e04ae120e8c5551f2ec7a7635
Toxic254 <cyrus_g@outlook.com>
2021-04-14 18:06:17 +0300

    Worked on data binding, using 'banana boat' model, working on adding pipes.

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -17,10 +17,23 @@ export class FormComponent implements OnInit {
   // we need a special module called FormsModule, to handle the forms and 
   // Reactive forms Module to helps us avert this problem.
 
+  // Here we create a userData Object that retrieves our user data, and displays 
+  // in the createUser() fn.
+  public userData = {
+    email: '',
+    password: ''
+  }
+
   // Create a function to simple handle user data, which essentially returns nothing.
-  private createUser(): void {
+  public createUser(): void {
     // TODO: Add Data Binding when submit in form is called.
     console.log("Created User.");
+    // Here we add data binding to our logs, where we bind what we get from our user and display it again.
+    // Now we bind it to our front end.
+    // The we use the banana boat...
+    console.log("User Email: ", this.userData.email);
+    console.log("User Password: ", this.userData.password);
+    
     
   }
 
