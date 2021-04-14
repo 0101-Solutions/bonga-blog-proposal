1bdd22720db62a758301cbada577281464221236
Toxic254 <cyrus_g@outlook.com>
2021-04-14 16:55:43 +0300

    Added ngOnInit.

--- src/app/_app/app.component.ts
+++ src/app/_app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 // Below is the component decorator which takes 3 arguments.
 @Component({
@@ -7,7 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-// The main logic behind the App Component 
-export class AppComponent {
+// The main logic behind the App Component. The class implements the OnInit Lifecycle Hook.
+// Mostly used and runs when everything is ready for initializing.
+export class AppComponent implements OnInit {
+  // This is an access to browser titles for each page
   title = 'shecodedemo';
+
+  // Initializing the variables in the class if any and create the class instance. It can be 
+  // parametized (can take parameters).
+  constructor() {
+    console.log('Parameters added.');
+    
+  }
+
+  // Then we have to initalize the On Init Method to make it work.
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  // We create a private function to be called only within the class not outside this component.
+  // It return nothing hence we say void.
+  private callMe(): void {
+    
+  }
 }
