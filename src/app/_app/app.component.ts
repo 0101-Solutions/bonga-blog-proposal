f63c9481d877302ad0eed4a54b9753b4cacc3e78
Toxic254 <cyrus_g@outlook.com>
2021-04-14 17:16:47 +0300

    Added templating on main component.

--- src/app/_app/app.component.ts
+++ src/app/_app/app.component.ts
@@ -10,8 +10,6 @@ import { Component, OnInit } from '@angular/core';
 // The main logic behind the App Component. The class implements the OnInit Lifecycle Hook.
 // Mostly used and runs when everything is ready for initializing.
 export class AppComponent implements OnInit {
-  // This is an access to browser titles for each page
-  title = 'shecodedemo';
 
   // Initializing the variables in the class if any and create the class instance. It can be 
   // parametized (can take parameters).
@@ -22,12 +20,29 @@ export class AppComponent implements OnInit {
 
   // Then we have to initalize the On Init Method to make it work.
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    console.log('Component is initialized.');
+    this.callMe();
   }
 
   // We create a private function to be called only within the class not outside this component.
   // It return nothing hence we say void.
   private callMe(): void {
+    console.log('Then our private method is called.');
     
   }
+  
+  title: string = 'She Code Ng-Webinar';
+
+  // Create an object.
+  blogObject = {
+    headline: "What is Typescript",
+    description: `TypeScript is a language that is a superset of JavaScript: JS syntax is therefore legal TS. Syntax refers to the way we write text to form a program. For example, this code has a syntax error because it’s missing a ):`
+  }
+
+  linksArray = [
+    "NodeJS For Beginners.",
+    "Gatsby 101.",
+    "How to Ace Your Technical Interview."
+  ];
+
 }
